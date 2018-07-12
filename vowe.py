d0ed190248c86c3354586040ace1e301746f945e
Harivigneshh <41141007+Harivigneshh@users.noreply.github.com>
2018-07-12 14:29:11 +0530

    Create vowe.py

---
+++ vowe.py
@@ -0,0 +1,8 @@
+x=str(input())
+if ord(x)>=97 and  ord(x)<=122:
+    if  x=="a" or x=="e" or x=="i" or x=="o" or x=="u":
+        print("Vowel")
+    else:
+        print("Consonant")
+else:
+    print("ivalid")
